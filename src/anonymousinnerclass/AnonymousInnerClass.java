c4c3fc3fc318dafb29c0e5d815d9494044ea03b0
Kristian Johansson <carlkristian.johansson@gmail.com>
2022-09-28 12:30:45 +0200

    Inner Class anónima

--- src/anonymousinnerclass/AnonymousInnerClass.java
+++ src/anonymousinnerclass/AnonymousInnerClass.java
@@ -7,7 +7,6 @@ package anonymousinnerclass;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
